a5d0b4870215162f86a9d479e32c8aafc08e44c9
Sandy Aditya <whitexoos.212@gmail.com>
2025-08-04 10:18:06 +0700

    Adjust responsiveness on mobile viewport and fix alert errors

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 let firstName, lastName, fullName, myAlert;
 
-document.getElementById('ButtonMerge').onclick = function () {
+document.getElementById('button-merge').onclick = function () {
     firstName = document.getElementById('first-name').value;
     lastName = document.getElementById('last-name').value;
     fullName = document.getElementById('full-name');
     myAlert = document.getElementById('alert');
+
     if (!firstName && !lastName) {
         myAlert.textContent = 'Please enter your full name!';
-        myAlert.classList.toggle('d-none');
+        myAlert.classList.remove('d-none');
     } else if (!firstName && lastName) {
         myAlert.textContent = 'Please enter your first name!';
-        myAlert.classList.toggle('d-none');
+        myAlert.classList.remove('d-none');
     } else if (firstName && !lastName) {
         myAlert.textContent = 'Please enter your last name!';
-        myAlert.classList.toggle('d-none');
+        myAlert.classList.remove('d-none');
     } else {
         fullName.style.fontWeight = 'bold';
         fullName.textContent = `${firstName} ${lastName}`;
+        myAlert.classList.add('d-none');
     }
 };
